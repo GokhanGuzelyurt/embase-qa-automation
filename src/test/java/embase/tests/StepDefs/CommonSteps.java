be056857c34163b4486294758ce22281513b9dbe
Diego Bruno <67111123+dbruno-elsevier@users.noreply.github.com>
2021-06-18 15:01:32 +0200

    Update CommonSteps.java

--- src/test/java/embase/tests/StepDefs/CommonSteps.java
+++ src/test/java/embase/tests/StepDefs/CommonSteps.java
@@ -5,39 +5,36 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import net.thucydides.core.annotations.Managed;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import po.BasePage;
+import po.ConfigPage;
+
+import java.lang.invoke.MethodHandles;
 
 public class CommonSteps {
 
+    final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     public static String EMB_BUILD_NUMBER;
 
     @Managed
     WebDriver driver;
 
     BasePage basePage;
+    ConfigPage configPage;
 
     @Before
     public void setUp() {
-        System.out.println("BEFORE");
+        logger.info("-- BEFORE --");
         getBuildNumber();
-
+        logger.info("Build number EMB_BUILD_NUMBER: " + EMB_BUILD_NUMBER);
     }
 
-    private void getBuildNumber() {
-//        if (EMB_BUILD_NUMBER == null) {
-//            logger.info("setUp - go to /config page");
-//            configPage.openUrl();
-//            logger.info("setUp - get the build number");
-//            configPage.getBuildNumber();
-//            // killing driver to keep the /config flow separate
-//            tearDown();
-//        } else {
-//            logger.info("build number already known: " + EMB_BUILD_NUMBER + " - skipping call to /config page");
-//        }
-    }
 
     @After
     public void tearDown() {
+        logger.info("-- AFTER --");
+        logger.info("Closing driver");
         driver.close();
         driver.quit();
         System.gc();
@@ -50,5 +47,16 @@ public class CommonSteps {
         System.out.println("OPEN PAGE");
     }
 
+    private void getBuildNumber() {
+        if (EMB_BUILD_NUMBER == null) {
+            logger.info("setUp - go to /config page");
+            configPage.open();
+            logger.info("setUp - get the build number");
+            EMB_BUILD_NUMBER = configPage.buildNumber.getText().split("build ")[1];
+            tearDown();
+        } else {
+            logger.info("build number already known");
+        }
+    }
 
 }
