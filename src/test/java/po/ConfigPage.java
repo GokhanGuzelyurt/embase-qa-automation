2ec3e4033c00215b30706e484a81698132b520b0
Diego Bruno <67111123+dbruno-elsevier@users.noreply.github.com>
2021-06-18 15:00:57 +0200

    config page

--- src/test/java/po/ConfigPage.java
+++ src/test/java/po/ConfigPage.java
@@ -1,11 +1,14 @@
 package po;
 
 import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-
+@DefaultUrl("page:config.page")
 public class ConfigPage extends BasePage {
 
+    @FindBy(xpath = "//p[contains(text(),'Version')]")
+    public WebElement buildNumber;
 
 }
