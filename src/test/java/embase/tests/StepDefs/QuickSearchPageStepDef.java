70a92b55a0365c17cb65612c5dda4b8074277831
Sakshi0289 <s.upadhyay@elsevier.com>
2021-11-18 17:49:40 +0100

    EMBASE-13733

--- src/test/java/embase/tests/StepDefs/QuickSearchPageStepDef.java
+++ src/test/java/embase/tests/StepDefs/QuickSearchPageStepDef.java
@@ -1,8 +1,12 @@
 package embase.tests.StepDefs;
 
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.commons.lang3.StringUtils;
 import po.QuickSearchPage;
+import org.assertj.core.api.Assertions;
+
 
 public class QuickSearchPageStepDef {
 
@@ -16,5 +20,21 @@ public class QuickSearchPageStepDef {
     @When("user opens Quick Search page")
     public void openQuickSearchPage() {
         quickSearchPage.open();
+    }
+
+    @Given("^user enters query (.*) on quick search page$")
+    public void enterQueryQuickSearch(String query) {
+        quickSearchPage.firstLine.click();
+        quickSearchPage.firstLine.sendKeys(query);
+    }
+
+    @Then("^the autosuggested term contains count greater than 10$")
+    public void verifyAutosuggestedCounts() {
+        Integer val;
+        String text = quickSearchPage.autosuggestList.getText();
+        String substring = StringUtils.substringAfterLast(text, "<");
+        substring= substring.replaceAll("\\s+","").replaceAll(",", "");
+        val = Integer.parseInt(substring);
+        Assertions.assertThat(val).describedAs("Value is not greater than 10").isGreaterThan(10);
     }
 }
