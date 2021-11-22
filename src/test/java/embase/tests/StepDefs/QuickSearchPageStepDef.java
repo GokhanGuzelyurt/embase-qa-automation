b35291a6046451fa2657ad39a673f2d8898a68a9
Diego Bruno <67111123+dbruno-elsevier@users.noreply.github.com>
2021-11-22 20:49:40 +0100

    missing space

--- src/test/java/embase/tests/StepDefs/QuickSearchPageStepDef.java
+++ src/test/java/embase/tests/StepDefs/QuickSearchPageStepDef.java
@@ -36,6 +36,6 @@ public class QuickSearchPageStepDef {
         String substring = StringUtils.substringAfterLast(text, "\n");
         substring = substring.replaceAll("//s+", "").replaceAll(",", "");
         val = Integer.parseInt(substring);
-        Assertions.assertThat(val).describedAs("Value is not greater than" + count).isGreaterThan(count);
+        Assertions.assertThat(val).describedAs("Value is not greater than " + count).isGreaterThan(count);
     }
 }
