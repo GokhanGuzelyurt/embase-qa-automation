3987f231470364272b1018bc8c4bf885dd591e81
Sakshi Upadhyay <s.upadhyay@elsevier.com>
2021-12-02 12:30:07 +0100

    removed the import

--- src/test/java/embase/tests/StepDefs/RecordDetailsStepDef.java
+++ src/test/java/embase/tests/StepDefs/RecordDetailsStepDef.java
@@ -3,7 +3,6 @@ package embase.tests.StepDefs;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import groovyjarjarantlr4.v4.analysis.LeftRecursiveRuleAnalyzer;
 import io.cucumber.datatable.DataTable;
 import org.assertj.core.api.Assertions;
 import po.RecordDetailsPage;
