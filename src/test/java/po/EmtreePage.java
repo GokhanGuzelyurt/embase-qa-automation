a159d5c9afc060b6b4d1dc58c7548572b84b29f5
Diego Bruno <67111123+dbruno-elsevier@users.noreply.github.com>
2022-02-04 15:45:16 +0100

    added test case coverage for GMDN terms in Emtree

---
+++ src/test/java/po/EmtreePage.java
@@ -0,0 +1,30 @@
+package po;
+
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import po.common.BasePage;
+
+import java.lang.invoke.MethodHandles;
+
+@DefaultUrl("page:emtree.page")
+public class EmtreePage extends BasePage {
+    final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @FindBy(id = "findTermInput")
+    public WebElement termInputTextField;
+
+    @FindBy(id = "findTermBtn")
+    public WebElement findTermButton;
+
+    @FindBy(css = ".findTermResults")
+    public WebElementFacade findTermResults;
+
+    @FindBy(css = "#emtreeWidget .emtree_content .additional")
+    public WebElement additionalEmtreeInformation;
+
+}
+
