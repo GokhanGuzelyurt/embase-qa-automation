8d519a4b1c4e1553d9ae279aebb4e65000061941
Diego Bruno <67111123+dbruno-elsevier@users.noreply.github.com>
2022-02-08 12:48:17 +0100

    Create Footer.java

---
+++ src/test/java/po/sections/Footer.java
@@ -0,0 +1,14 @@
+package po.sections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import po.common.BasePage;
+
+import java.lang.invoke.MethodHandles;
+
+public class Footer extends BasePage {
+    final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+
+
+}
